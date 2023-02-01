fe91fd058ab73add5feb4678d0cec3bad773bb59
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2023-02-01 22:39:16 +0100

    e2e TestCustomerAddressRelationalProject.java

--- e2e/src/test/java/io/zenwave360/sdk/e2e/TestCustomerAddressRelationalProject.java
+++ e2e/src/test/java/io/zenwave360/sdk/e2e/TestCustomerAddressRelationalProject.java
@@ -15,49 +15,28 @@ import io.zenwave360.sdk.plugins.OpenAPIControllersPlugin;
 import io.zenwave360.sdk.plugins.ZDLToAsyncAPIPlugin;
 import io.zenwave360.sdk.plugins.ZDLToOpenAPIPlugin;
 import io.zenwave360.sdk.testutils.MavenCompiler;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCustomerAddressRelationalProject {
-
-    private static String project = "customer-address-relational";
-    private static String sourceFolder = "src/test/resources/projects/" + project + "/";
-    private static String targetFolder = "target/projects/" + project + "/";
     private String basePackage = "io.zenwave360.example";
-    private String zdlFile = targetFolder + "/customer-address-relational.zdl";
 
-    @BeforeAll
-    public static void beforeAll() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"one-to-many", "one-to-one", "one-to-one-aggregates", "one-to-one-map-id", "many-to-one"})
+    public void testCustomerAddressRelational(String flavor) throws Exception {
+        String sourceFolder = "src/test/resources/projects/customer-address-relational/";
+        String targetFolder = "target/projects/customer-address-relational/" + flavor;
+        String zdlFile = targetFolder + "/customer-address-relational-" + flavor + ".zdl";
+
         // copy whole dir from sourceFolder to targetFolder
         FileUtils.deleteDirectory(new File(targetFolder));
         FileUtils.forceMkdir(new File(targetFolder));
         FileUtils.copyDirectory(new File(sourceFolder), new File(targetFolder));
         Assertions.assertTrue(new File(targetFolder).exists());
-    }
 
-//        @Test
-    public void test() throws Exception {
-        Plugin plugin = new BackendApplicationDefaultPlugin()
-                .withSpecFile(zdlFile)
-                .withTargetFolder(targetFolder)
-                .withOption("basePackage", basePackage)
-                .withOption("persistence", PersistenceType.jpa)
-                .withOption("databaseType", DatabaseType.mariadb)
-                .withOption("style", ProgrammingStyle.imperative)
-                .withOption("useLombok", true)
-                .withOption("includeEmitEventsImplementation", true)
-                .withOption("forceOverwrite", true)
-                .withOption("haltOnFailFormatting", false);
-
-
-        new MainGenerator().generate(plugin);
-        //        int exitCode = MavenCompiler.compile(new File(targetFolder));
-        //        Assertions.assertEquals(0, exitCode);
-    }
-
-    @Order(1)
-    @Test
-    public void generateApis() throws Exception {
         Plugin plugin = null;
+        int exitCode = 0;
 
         plugin = new ZDLToOpenAPIPlugin()
                 .withSpecFile(zdlFile)
@@ -88,20 +67,6 @@ public class TestCustomerAddressRelationalProject {
                 .withOption("targetFile", "/src/main/resources/apis/asyncapi.yml")
                 .withTargetFolder(targetFolder);
         new MainGenerator().generate(plugin);
-    }
-
-    @Order(2)
-    @Test
-    public void generateSourceFromAPIs() throws Exception {
-        int exitCode = MavenCompiler.compile(new File(targetFolder));
-        Assertions.assertEquals(0, exitCode);
-    }
-
-    @Order(3)
-    @Test
-    public void generateModule() throws Exception {
-        Plugin plugin = null;
-        int exitCode = 0;
 
         plugin = new BackendApplicationDefaultPlugin()
                 .withSpecFile(zdlFile)
