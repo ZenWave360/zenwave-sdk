c9a22174d3cc9b48aee15b311f0761ae516d1fd3
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2024-03-29 12:56:32 +0100

    e2e tests for monolithic project

--- e2e/src/test/java/io/zenwave360/sdk/e2e/TestMonolithClinicalProject.java
+++ e2e/src/test/java/io/zenwave360/sdk/e2e/TestMonolithClinicalProject.java
@@ -53,12 +53,17 @@ public class TestMonolithClinicalProject {
     @ParameterizedTest
     @CsvSource({
             "clinical.zdl, webapp, getPatientProfileById, ",
-            "clinical.zdl, mobile, , 'getPatientProfileById,requestOptOut'"
+            "clinical.zdl, mobile, , 'getPatientProfileById,requestOptOut'",
+            "documents.zdl, documents, , ",
+            "surveys.zdl, surveys-backoffice, 'getSurveyAndQuestionsForPatient,answerSurvey,updateSurveyAnswers,getSurveyAnswers',",
+            "surveys.zdl, surveys-public, , 'getSurveyAndQuestionsForPatient,answerSurvey,updateSurveyAnswers,getSurveyAnswers'",
+            "masterdata.zdl, masterdata, , ",
+            "terms-and-conditions.zdl, terms-and-conditions, ,"
     })
     @Order(1)
     public void testGenerateOpenAPIs(String zdlFileNames, String webModule, String operationIdsToExclude, String operationIdsToInclude) throws Exception {
         List<String> zdlFiles = List.of(zdlFileNames.split(",")).stream().map(zdlFileName -> targetFolder + "/models/" + zdlFileName).toList();
-        String openApiFile = targetFolder + "/src/main/resources/apis/" + webModule + "-openapi.yml";
+        String openApiFile = targetFolder + "/src/main/resources/public/apis/" + webModule + "-openapi.yml";
 
         Plugin plugin = new ZDLToOpenAPIPlugin()
                 .withZdlFiles(zdlFiles)
@@ -73,7 +78,10 @@ public class TestMonolithClinicalProject {
     @ParameterizedTest
     @CsvSource({
             "clinical.zdl",
-            "surveys.zdl"
+            "documents.zdl",
+            "surveys.zdl",
+            "masterdata.zdl",
+            "terms-and-conditions.zdl"
     })
     @Order(2)
     public void testGenerateBackendModules(String zdlFile) throws Exception {
@@ -82,30 +90,37 @@ public class TestMonolithClinicalProject {
                 .withTargetFolder(targetFolder)
                 .withOptions(options);
         new MainGenerator().generate(plugin);
-
-//        int exitCode = MavenCompiler.compile(new File(targetFolder));
-//        Assertions.assertEquals(0, exitCode);
     }
 
     @Test
     @Order(3)
+    @Disabled
     public void compileBackendModules() throws Exception {
         int exitCode = MavenCompiler.compile(new File(targetFolder));
         Assertions.assertEquals(0, exitCode);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "clinical.zdl,webapp-openapi.yml,webapp",
+            "clinical.zdl,mobile-openapi.yml,mobile",
+            "documents.zdl,documents-openapi.yml,documents",
+            "surveys.zdl,surveys-backoffice-openapi.yml,surveys.backoffice",
+            "surveys.zdl,surveys-public-openapi.yml,surveys.api",
+            "masterdata.zdl,masterdata-openapi.yml,masterdata",
+            "terms-and-conditions.zdl,terms-and-conditions-openapi.yml,termsandconditions"
+    })
     @Order(4)
-    public void testGenerateControllers() throws Exception {
-        String zdlFile = targetFolder + "/models/clinical.zdl";
-        String openApiFile = targetFolder + "/src/main/resources/apis/webapp-openapi.yml";
+    public void testGenerateControllers(String zdl, String openapi, String webModule) throws Exception {
+        String zdlFile = targetFolder + "/models/" + zdl;
+        String openApiFile = targetFolder + "/src/main/resources/public/apis/" + openapi;
 
         Plugin plugin = new OpenAPIControllersPlugin()
                 .withZdlFile(zdlFile)
                 .withApiFile(openApiFile)
                 .withTargetFolder(targetFolder)
                 .withOptions(options)
-                .withOption("customWebModule", "{{basePackage}}.adapters.web.webapp")
+                .withOption("customWebModule", "{{basePackage}}.adapters.web." + webModule)
                 .withOption("layout.adaptersWebPackage", "{{customWebModule}}")
                 .withOption("layout.openApiApiPackage", "{{layout.customWebModule}}")
         ;
@@ -114,6 +129,15 @@ public class TestMonolithClinicalProject {
 
     @Test
     @Order(5)
+    public void fixControllers() throws Exception {
+
+        TextUtils.replaceInFile(new File(targetFolder + "/src/main/java/com/example/clinical/adapters/web/masterdata/MasterDataApiController.java"),
+                "masterDataService.listMasterDataOfType\\(type, lang\\)",
+                "masterDataService.listMasterDataOfType(MasterDataType.valueOf(type), lang)");
+    }
+
+    @Test
+    @Order(6)
     public void compileControllers() throws Exception {
         int exitCode = MavenCompiler.compile(new File(targetFolder));
         Assertions.assertEquals(0, exitCode);
