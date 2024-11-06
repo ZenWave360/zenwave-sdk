a894d25b03936033f761a6284de8c616d704ae3e
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2024-11-06 12:53:42 +0100

    [backend-application-default] adds support for oracle, mysql and generic DatabaseType

--- zenwave-sdk-cli/src/main/java/io/zenwave360/sdk/options/DatabaseType.java
+++ zenwave-sdk-cli/src/main/java/io/zenwave360/sdk/options/DatabaseType.java
@@ -1,6 +1,21 @@
 package io.zenwave360.sdk.options;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum DatabaseType {
+    generic,
     postgresql,
+    mysql,
     mariadb,
+    oracle;
+
+    @JsonCreator
+    public static DatabaseType fromValue(String value) {
+        for (DatabaseType type : DatabaseType.values()) {
+            if (type.name().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        return generic;
+    }
 }
