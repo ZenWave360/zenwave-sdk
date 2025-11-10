3e94b0a5caaf58abc0dcde85bfd1f781d8f7fe6e
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-11-10 21:28:19 +0200

    asyncapi-generator: kafka producer

--- plugins/asyncapi-generator/src/main/java/io/zenwave360/sdk/plugins/templates/SpringCloudStreamTemplates.java
+++ plugins/asyncapi-generator/src/main/java/io/zenwave360/sdk/plugins/templates/SpringCloudStreamTemplates.java
@@ -22,7 +22,7 @@ public class SpringCloudStreamTemplates extends AbstractAsyncapiGenerator.Templa
         }
         addTemplate(producerByServiceTemplates, "shared/producer/mocks/InMemoryEventsProducer.java", "src/test/java/{{asPackageFolder producerApiPackage}}/{{producerInMemoryName serviceName operationRoleType}}.java");
 
-        addTemplate(consumerByOperationTemplates, "shared/consumer/{{style}}/IService.java", "src/main/java/{{asPackageFolder consumerApiPackage}}/{{consumerServiceInterfaceName operation.x--operationIdCamelCase}}.java");
+        addTemplate(consumerByChannelTemplates, "shared/consumer/{{style}}/IServiceByChannel.java", "src/main/java/{{asPackageFolder consumerApiPackage}}/{{consumerServiceInterfaceName (camelCase channelName)}}.java");
         addTemplate(consumerByChannelTemplates, "scs/consumer/{{style}}/Consumer.java", "src/main/java/{{asPackageFolder consumerApiPackage}}/{{consumerName (camelCase channelName)}}.java");
     }
 }
